9874572d36a0bd06d5262869159c636669eba019
Stephan <stephan.x.williams@pwc.com>
2018-07-18 16:55:21 +0100

    Added random favicon, added filter buttons with no functionality

---
+++ RumourMill/Content/Filtering.js
@@ -0,0 +1,35 @@
+﻿(function ($) {
+
+    'use strict';
+
+    var $filters = $('.filter [data-filter]'),
+        $boxes = $('.cd-timeline__container [data-color]');
+
+    $filters.on('click', function (e) {
+        e.preventDefault();
+        var $this = $(this);
+
+        $filters.removeClass('active');
+        $this.addClass('active');
+
+        var $filterColor = $this.attr('data-filter');
+
+
+        var $filterColor2 = $this.attr('data-answered');
+
+        if ($filterColor == 'all') {
+            $boxes.removeClass('is-animated')
+                .fadeOut().promise().done(function () {
+                    $boxes.addClass('is-animated').fadeIn();
+                });
+        } else {
+            $boxes.removeClass('is-animated')
+                .fadeOut().promise().done(function () {
+                    $boxes.filter('[data-color = "' + $filterColor + '"]')
+                        .addClass('is-animated').fadeIn();
+                    
+                });
+        }
+    });
+
+})(jQuery);
