902b1a1b68178e1f5ca09140d21e2f3616846eee
Stephan <stephan.x.williams@pwc.com>
2018-08-13 11:27:56 +0100

    Fixes for edit field showing

---
+++ RumourMill/Scripts/main.js
@@ -0,0 +1,21 @@
+﻿$('.reason').change(function () {
+
+    var thisQID = $(this).attr("class");
+
+    var str = thisQID.substring(15, thisQID.length);
+
+    var edited = thisQID.substring(7, 8);
+
+    console.log("Class ID: " + str +". Edited: " +edited);
+
+
+    if ($(this).val() == "Other" || edited==0) {
+        $('.edittextarea' + str).attr('required');
+
+        $('.edittextarea' + str).css("display", "block");
+    } else {
+        $('.edittextarea' + str).removeAttr('required');
+
+        $('.edittextarea' + str).css("display", "none");
+    }
+});
